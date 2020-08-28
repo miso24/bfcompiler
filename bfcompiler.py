47ccfbc65dc99dc1d682da97d42373dcf4ff2579
宮崎想也 <green-24@outlook.jp>
2020-08-28 15:51:38 +0900

    fix bfcompiler.py

--- bfcompiler.py
+++ bfcompiler.py
@@ -5,7 +5,7 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", type=str, nargs="+", help="output file name", metavar=("out1", "out2"))
+    parser.add_argument("-o", type=str, nargs="+", help="output file name", default=[], metavar=("out1", "out2"))
     parser.add_argument("infile", type=str, nargs="+", help="brainfuck source")
     args = parser.parse_args()
 
@@ -14,9 +14,7 @@ if __name__ == "__main__":
         with open(file) as f:
             data = f.read()
         binary = compiler.compile(data)
-        if out_name is None:
+        if out_name is not None:
             save_as_elf(binary, out_name)
         else:
             save_as_elf(binary, file[:file.rfind(".")])
-
-    print(args)
