cd58b5d6cb590e74e12517531773ca02a60a31dc
宮崎想也 <green-24@outlook.jp>
2020-08-28 16:00:01 +0900

    add memsize argument

--- bfcompiler.py
+++ bfcompiler.py
@@ -7,9 +7,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", type=str, nargs="+", help="output file name", default=[], metavar=("out1", "out2"))
     parser.add_argument("infile", type=str, nargs="+", help="brainfuck source")
+    parser.add_argument("-m", "--mem", type=int, help="memory size")
     args = parser.parse_args()
 
-    compiler = Compiler()
+    if args.mem:
+        mem_size = args.mem
+    else:
+        mem_size = 0x100
+    compiler = Compiler(mem_size=mem_size)
     for file, out_name in zip_longest(args.infile, args.o):
         with open(file) as f:
             data = f.read()
